d963be031e97695211c0aa24986bbbebf8cd2702
Ivo Meixner <natiwastaken@gmail.com>
2020-01-22 13:45:14 +0100

    Fix #45: Add docstrings to data_pipeline_error

--- controller/data_pipeline_error.py
+++ controller/data_pipeline_error.py
@@ -1,5 +1,14 @@
+"""Module containing the implementation of the `DataPipelineError` exception class."""
+
+
 class DataPipelineError(Exception):
-    """Represents data pipeline exceptions."""
+    """
+    Represents a data pipeline exception.
+
+    This should make it easier to differentiate between
+    exceptions caused by internal and external code.
+    """
 
     def __init__(self, *args, **kwargs):
+        """Construct a new data pipeline exception instance."""
         super().__init__(*args, **kwargs)
