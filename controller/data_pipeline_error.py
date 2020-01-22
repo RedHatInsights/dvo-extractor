74cd3b7c4d4a87dc499fffaf22002bf2aaba1599
Ivo Meixner <natiwastaken@gmail.com>
2020-01-22 13:49:47 +0100

    Remove useless super delegation from D.P.E.

--- controller/data_pipeline_error.py
+++ controller/data_pipeline_error.py
@@ -8,7 +8,3 @@ class DataPipelineError(Exception):
     This should make it easier to differentiate between
     exceptions caused by internal and external code.
     """
-
-    def __init__(self, *args, **kwargs):
-        """Construct a new data pipeline exception instance."""
-        super().__init__(*args, **kwargs)
