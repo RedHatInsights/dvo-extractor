9dccfabf9ebf23c66e8a75e4c5647479f35a842e
Ivo Meixner <natiwastaken@gmail.com>
2020-02-03 18:44:36 +0100

    Fix #96: Add support for config via env var

--- controller/publisher.py
+++ controller/publisher.py
@@ -20,7 +20,17 @@ class Publisher(Publisher):
 
     def __init__(self, **kwargs):
         """Construct a new `Publisher` given `kwargs` from the config YAML."""
-        self.topic = kwargs.pop('outgoing_topic')
+        fallback_topic = kwargs.pop('outgoing_topic', None)
+        topic_env = kwargs.pop('outgoing_topic_env', None)
+        self.topic = os.environ.get(topic_env, fallback_topic) \
+            if topic_env is not None else fallback_topic
+
+        server_env = kwargs.pop('bootstrap_server_env', None)
+        if server_env is not None:
+            env_server = os.environ.get(server_env, None)
+            if env_server is not None:
+                kwargs['boostrap_servers'] = [env_server]
+
         self.producer = KafkaProducer(**kwargs)
 
     def publish(self, input_msg, response):
