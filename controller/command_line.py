1ed749da4806a49aa0a4ad0a20231510fd3a3807
José Luis Segura Lucas <joseluis@redhat.com>
2020-02-05 10:51:17 +0100

    Pydocstyle fixes

--- controller/command_line.py
+++ controller/command_line.py
@@ -6,12 +6,14 @@ from insights_messaging.appbuilder import AppBuilder
 
 
 def parse_args():
+    """Parse the arguments passed to the `main` entry point."""
     p = argparse.ArgumentParser()
     p.add_argument("config", help="Application Configuration.")
     return p.parse_args()
 
 
 def main():
+    """Entry point function handling the command `ccx-data-pipeline`."""
     args = parse_args()
 
     prometheus_client.start_http_server(8000)
