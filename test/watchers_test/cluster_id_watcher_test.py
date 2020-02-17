133d5b2647175e2bd645e5c20d287f0369874363
José Luis Segura Lucas <joseluis@redhat.com>
2020-02-17 16:36:22 +0100

    Checking cluster name is a UUID

---
+++ test/watchers_test/cluster_id_watcher_test.py
@@ -0,0 +1,84 @@
+"""Module containing unit tests for the `ConsumerWatcher` class."""
+
+from unittest.mock import MagicMock, mock_open, patch
+
+import pytest
+
+from controller.watchers.cluster_id_watcher import ClusterIdWatcher
+
+
+def _mock_input_msg():
+    class InputMessageDouble:
+        value = dict()
+
+    return InputMessageDouble()
+
+
+def test_cluster_id_watcher_unordered_events(caplog):
+    """Test that an extraction event is received without a receiving one ."""
+    sut = ClusterIdWatcher()
+    sut.on_extract(None, None, None)
+
+    assert sut.last_record is None
+    assert "Unexpected data flow" in caplog.text
+
+
+def test_cluster_id_watcher_file_not_exist(caplog):
+    """Test that the extracted archive doesn't contain a cluster id file."""
+    input_msg_mock = MagicMock()
+    input_msg_mock.value = dict()
+
+    ctx_mock = MagicMock()
+    ctx_mock.root = '/tmp/to/non/existing/file/over/the/filesystem'
+
+    sut = ClusterIdWatcher()
+    sut.on_recv(input_msg_mock)
+
+    sut.on_extract(ctx_mock, None, None)
+
+    assert input_msg_mock.value['ClusterName'] is None
+    assert "The archive doesn't contain a valid Cluster Id file" in caplog.text
+
+
+_INCORRECT_UUIDS = [
+    "0",
+    "---//---",
+    "hhhh-bbbb-cccc-dddd-000000000000",
+]
+
+
+@pytest.mark.parametrize("value", _INCORRECT_UUIDS)
+def test_cluster_id_watcher_bad_content(caplog, value):
+    """Test that the id file contains non UUID values."""
+    input_msg_mock = MagicMock()
+    input_msg_mock.value = dict()
+
+    ctx_mock = MagicMock()
+    ctx_mock.root = '/tmp/mock/path'
+
+    sut = ClusterIdWatcher()
+    sut.on_recv(input_msg_mock)
+
+    with patch("builtins.open", mock_open(read_data=value)):
+        sut.on_extract(ctx_mock, None, None)
+        assert input_msg_mock.value['ClusterName'] is None
+        assert "The cluster id is not an UUID" in caplog.text
+
+
+def test_cluster_id_watcher_ok(caplog):
+    """Test that the id file contains non UUID values."""
+    input_msg_mock = MagicMock()
+    input_msg_mock.value = dict()
+
+    ctx_mock = MagicMock()
+    ctx_mock.root = '/tmp/mock/path'
+
+    sut = ClusterIdWatcher()
+    sut.on_recv(input_msg_mock)
+
+    uuid_value = "aaaaaaaa-bbbb-cccc-dddd-000000000000"
+
+    with patch("builtins.open", mock_open(read_data=uuid_value)):
+        sut.on_extract(ctx_mock, None, None)
+        assert input_msg_mock.value['ClusterName'] == uuid_value
+        assert len(caplog.records) == 0
