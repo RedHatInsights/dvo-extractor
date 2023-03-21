d528eeae6d0c8708f071435633f33ff0cf829db9
José Luis Segura Lucas <joseluis@redhat.com>
2023-03-21 13:09:17 +0100

    Fix linter

--- test/command_line_test.py
+++ test/command_line_test.py
@@ -49,7 +49,6 @@ def test_command_line_args_invalid_arg_provided(capsys):
     """Verify help is shown if invalid argument provided."""
     sys.argv = ["ccx-data-pipeline", "--config"]
     with pytest.raises(SystemExit) as exception:
-
         parser = command_line.parse_args()
 
         assert exception.type == SystemExit
